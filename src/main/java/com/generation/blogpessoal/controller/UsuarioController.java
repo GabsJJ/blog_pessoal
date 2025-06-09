44d3ed8894f8c32687a61ac55f2a45ad3f4d4ebc
Gabriel Julio <gabrieldasilvajulio@gmail.com>
2025-06-09 03:44:08 -0300

    :rocket: Deploy do Projeto Blog Pessoal e finalização geral

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -1,19 +1,23 @@
 package com.generation.blogpessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.model.Usuario;
 import com.generation.blogpessoal.model.UsuarioLogin;
+import com.generation.blogpessoal.repository.UsuarioRepository;
 import com.generation.blogpessoal.service.UsuarioService;
 
 import jakarta.validation.Valid;
@@ -25,6 +29,15 @@ public class UsuarioController {
     
     @Autowired
     private UsuarioService usuarioService; //onde está o método cadastrar
+    
+    @Autowired
+	private UsuarioRepository usuarioRepository;
+    
+    @GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+
+	}
     
     @PostMapping("/cadastrar") //endpoint para cadastro - libera o uso sem autenticação
     public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario usuario){
